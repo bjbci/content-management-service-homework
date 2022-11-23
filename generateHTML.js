ad8af1aba650c5a4cd566f22e1a98bf4b530bfa7
Brian Bulowicz <bajebi.bb@gmail.com>
2022-11-23 00:52:16 -0600

    fourth commit cki part done

--- generateHTML.js
+++ generateHTML.js
@@ -1,6 +1,10 @@
 const generateHTML= (data)=>{
-    console.log(data)
+    //console.log(data)
 
+    //deconstruct data  --we are pulling these 'varables' out of (data)
+    const {intro, what_department,role_name,
+        department_belong ,salary,first_name,
+        last_name,employee_role,employee_manager}=data
     return`   
     <!DOCTYPE html>
 <html lang="en">
@@ -15,27 +19,23 @@ const generateHTML= (data)=>{
 
     <hi>CONTENT MANAGEMENT SYSTEM</hi>
 
-    <H3>What would you like to do?  </H3>
-    <choices>Add Department  Add Role  Add Employee  Update Employee Role</choices>
+    <H3>What would you like to do? ${intro } </H3>
+    
+    <H3>What Is Your Department?  ${what_department } </H3>
+    
+    <H3>What Is The Name Of The Role?  ${role_name } </H3>
+    
+    <H3>What Department Does This Role Belong To? ${department_belong  }  </H3>
+    
+    <H3>What Is The Salary Of The Role?  ${salary } </H3>
 
-    <H3>What Is Your Department?  </H3>
-    <choices>Sales  Engineering  Accounts Legal  </choices>
+    <H3>What Is The Employees First Name? ${first_name}  </H3>
 
-    <H3>What Is The Name Of The Role?  </H3>
-    <choices>Salesperson Lead-Engineer Software-Engineer Account-Manager Accountant  Legal-Team-Lead Lawyer   </choices>
+    <H3>What Is The Employees Last Name? ${last_name } </H3>
+    
+    <H3>What Is The Employees Role? ${employee_role }  </H3>
 
-    <H3>What Department Does This Role Belong To?  </H3>
-    <choices> Sales  Engineering  Accounts Legal  </choices>
-
-    <H3>What Is The Salary Of The Role?  </H3>
-
-    <H3>What Is The Employees First Name?  </H3>
-
-    <H3>What Is The Employees Last Name? </H3>
-
-    <H3>What Is The Employees Role?  </H3>
-
-    <H3>Who Is The Employees Manager?  </H3>
+    <H3>Who Is The Employees Manager? ${employee_manager }  </H3>
 
 
 </body>
@@ -43,4 +43,5 @@ const generateHTML= (data)=>{
     `
 }
 
-module.export=generateHTML
+// module.export=generateHTML   wont work because it is es5-need es6
+export default generateHTML
--- index.js
+++ index.js
@@ -37,3 +37,98 @@
 // * Delete departments, roles, and employees.
 
 // * View the total utilized budget of a department&mdash;in other words, the combined salaries of all employees in that department.
+
+
+// inquirer
+//   .prompt([
+    //     /* Pass your questions in here */
+    //   ])
+    //   .then((answers) => {
+        //     // Use user feedback for... whatever!!
+        //   })
+        //   .catch((error) => {
+            //     if (error.isTtyError) {
+                //       // Prompt couldn't be rendered in the current environment
+                //     } else {
+                    //       // Something else went wrong
+                    //     }
+                    //   });
+                    
+    import inquirer from 'inquirer'  //install inquirer from npm
+    // console.log(inquirer)
+    import generateHTML from './generateHTML.js' //relative path needed for self made modules!
+    //console.log(generateHTML)
+    import fs from "fs"  //probably dont need this for this assignment
+     inquirer.prompt([
+
+    {
+        type: "list" ,
+        message:"What would you like to do?"  ,
+        name:   "intro",
+        choices:[" Add Department",  "Add Role",  "Add Employee",  "Update Employee Role "]
+    },
+    {
+        type: "list" ,
+        message:"What Is Your Department?   " ,
+        name:  "what_department" ,
+        choices:[" Sales", "Engineering",  "Accounts Legal"]
+    },
+    {
+        type: "list" ,
+        message: "What Is The Name Of The Role?  "  ,
+        name:   "role_name",
+        choices: ["Salesperson", "Lead-Engineer", "Software-Engineer", "Account-Manager","Accountant",  "Legal-Team-Lead", "Lawyer"]
+    },
+    {
+        type: "list" ,
+        message: "What Department Does This Role Belong To?  ",
+        name:  "department_belong" ,
+        choices: [" Sales",  "Engineering",  "Accounts", "Legal"]
+    },
+    {
+        type: "input" ,
+        message: "What Is The Salary Of The Role?  "  ,
+        name:  "salary"
+    },
+    {
+        type: "input" ,
+        message:  "What Is The Employees First Name?  " ,
+        name:  "first_name"
+    },
+    {
+        type: "input" ,
+        message: "What Is The Employees Last Name?  "  ,
+        name:  "last_name" 
+    },
+    {
+        type: "list" ,
+        message:"What Is The Employees Role?   "   ,
+        name:   "employee_role",
+        choices:["Salesperson", "Lead-Engineer", "Software-Engineer", "Account-Manager", "Accountant",  "Legal-Team-Lead", "Lawyer"]
+    },
+    {
+        type: "input" ,
+        message: "Who Is The Employees Manager?  "  ,
+        name: "employee_manager"
+    }
+    // {
+    //     type:  ,
+    //     message: "   "  ,
+    //     name:   
+    // }
+  ])
+    .then(answers =>{
+        console.log(answers)
+        //generateHTML string
+        const html=generateHTML(answers) //the big html string will be save in the const html
+        console.log(html)
+        //save the file---PROBABLY DONT NEEED THIS FOR THIS ASSIGNMENT
+        fs.writeFile('./content-management-system.html',html,error=>{
+            if(error) throw error
+
+            console.log('html saved')
+        })
+    })
+    .catch(error => console.log(error))
+
+  
